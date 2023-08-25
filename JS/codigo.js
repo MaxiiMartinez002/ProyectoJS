608aca7767d8cbf063e947cc46d4ca879ceb851e
Maximiliano Martinez <maxim30600@gmail.com>
2023-08-24 22:27:26 -0300

    entregaFinal

--- JS/codigo.js
+++ JS/codigo.js
@@ -7,54 +7,61 @@ const cantidadCarrito = document.getElementById("cantidadCarrito");
 
 let carrito = JSON.parse(localStorage.getItem("carrito")) || [];
 
-productos.forEach((product) => {
-  let content = document.createElement("div");
-  content.className = "card";
-  content.innerHTML = `
-    <img src="${product.imagen}">
-    <h3>${product.nombre}</h3>
-    <p class="price">${product.precio} $</p>
-  `;
-
-  shopContent.append(content);
-
-  let comprar = document.createElement("button");
-  comprar.innerText = "comprar";
-  comprar.className = "comprar";
-
-  content.append(comprar);
-
-  comprar.addEventListener("click", () => {
-    const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
-
-    if (repeat) {
-      carrito.map((prod) => {
-        if (prod.id === product.id) {
-          prod.cantidad++;
-        }
-      });
-    } else {
-      carrito.push({
-        id: product.id,
-        img: product.imagen,
-        nombre: product.nombre,
-        precio: product.precio,
-        cantidad: product.cantidad,
-      });
-      console.log(carrito);
-      console.log(carrito.length);
-      carritoCounter();
-      saveLocal();
-    }
-    Swal.fire({
-        position: 'top-end',
-        icon: 'success',
-        title: `Agregaste ${product.nombre} al carrito`,
-        showConfirmButton: false,
-        timer: 1500,
-      })
+const getProductos = async () => {
+  const response = await fetch("data.json");
+  const data = await response.json();
+  data.forEach((product) => {
+    let content = document.createElement("div");
+    content.className = "card";
+    content.innerHTML = `
+      <img src="${product.imagen}">
+      <h3>${product.nombre}</h3>
+      <p class="price">${product.precio} $</p>
+    `;
+  
+    shopContent.append(content);
+  
+    let comprar = document.createElement("button");
+    comprar.innerText = "comprar";
+    comprar.className = "comprar";
+  
+    content.append(comprar);
+  
+    comprar.addEventListener("click", () => {
+      const repeat = carrito.some((repeatProduct) => repeatProduct.id === product.id);
+  
+      if (repeat) {
+        carrito.map((prod) => {
+          if (prod.id === product.id) {
+            prod.cantidad++;
+          }
+        });
+      } else {
+        carrito.push({
+          id: product.id,
+          img: product.imagen,
+          nombre: product.nombre,
+          precio: product.precio,
+          cantidad: product.cantidad,
+        });
+        console.log(carrito);
+        console.log(carrito.length);
+        carritoCounter();
+        saveLocal();
+      }
+      Swal.fire({
+          position: 'top-end',
+          icon: 'success',
+          title: `Agregaste ${product.nombre} al carrito`,
+          showConfirmButton: false,
+          timer: 1500,
+        })
+    });
   });
-});
+}
+getProductos();
+
+
 
 const saveLocal = () => {
   localStorage.setItem("carrito", JSON.stringify(carrito));
@@ -74,3 +81,4 @@ const saveLocal = () => {
 
 
 
+ 
